311d261aa848f79a399eeb701aa05139ae950306
Victor Bezak <victorjbezak@gmail.com>
2018-10-28 17:17:48 -0500

    One possible execution of our dynamic array initialization. This should all go into a separate function, and also it would be nice to have this performed with a single num variable in a loop.

--- Homework6/main.c
+++ Homework6/main.c
@@ -13,20 +13,63 @@
 
 
 #include "stdio.h"
+#include "stdlib.h"
 
 
 // MAIN BEGIN
 int main()
 {
-   int num;
+   int num1, num2, num3, num4, num5, num6, num7, num8;
+   int *bblsort = NULL;    // Declare pointer for bubble sort dynamic array
+   int *selectsort = NULL; // Declare pointer for selection sort dynamic array
    char pause;
    
-   printf("Please enter a number: ");
-   scanf("%d", &num);
+   bblsort    = (int*)calloc(8, sizeof(int)); // Allocate memory for int array
+   selectsort = (int*)calloc(8, sizeof(int)); // Allocate memory for int array
    
-   printf("Hello, World! We got a %d!\n", num);
+   puts("Please enter eight integers...");
    
-   scanf("%c%*c", &pause); // To pause while a dangling newline is present
+   printf("int 1: ");
+   scanf("%d", &num1);
+   bblsort[0]    = num1;
+   selectsort[0] = num1;
+   
+   printf("int 2: ");
+   scanf("%d", &num2);
+   bblsort[1]    = num2;
+   selectsort[1] = num2;
+   
+   printf("int 3: ");
+   scanf("%d", &num3);
+   bblsort[2]    = num3;
+   selectsort[2] = num3;
+   
+   printf("int 4: ");
+   scanf("%d", &num4);
+   bblsort[3]    = num4;
+   selectsort[3] = num4;
+   
+   printf("int 5: ");
+   scanf("%d", &num5);
+   bblsort[4]    = num5;
+   selectsort[4] = num5;
+   
+   printf("int 6: ");
+   scanf("%d", &num6);
+   bblsort[5]    = num6;
+   selectsort[5] = num6;
+   
+   printf("int 7: ");
+   scanf("%d", &num7);
+   bblsort[6]    = num7;
+   selectsort[6] = num7;
+   
+   printf("int 8: ");
+   scanf("%d", &num8);
+   bblsort[7]    = num8;
+   selectsort[7] = num8;
+   
+   scanf("%c%*c", &pause);
    
    return 0;
 } // MAIN END
