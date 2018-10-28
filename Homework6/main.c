cbe53e86043b6d5a47e7a9f3cbc2ca93922c8883
Victor Bezak <victorjbezak@gmail.com>
2018-10-28 12:44:46 -0500

    Got basic outline ready with some refresher/practice input and output code. Ready for construction!

--- Homework6/main.c
+++ Homework6/main.c
@@ -1,15 +1,32 @@
-//
-//  main.c
-//  Homework6
-//
-//  Created by Victor Bezak III on 10/28/18.
-//  Copyright © 2018 University of Texas at Dallas. All rights reserved.
-//
+/*
+ Author:      Victor J. Bezak III
+ Net ID:      vjb170000
+ Date:        October 28th, 2018
+ Course:      CS 1337.001
+ Assignment:  Homework 6
+ 
+ Description:
+ Will create 2 dynamic 8 int arrays from user input. Displays these arrays before,
+ during, and after sorting--sorting array 1 with a bubble sort and array 2 with a
+ selection sort.
+ */
 
-#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-   // insert code here...
-   printf("Hello, World!\n");
+#include "stdio.h"
+
+
+// MAIN BEGIN
+int main()
+{
+   int num;
+   char pause;
+   
+   printf("Please enter a number: ");
+   scanf("%d", &num);
+   
+   printf("Hello, World! We got a %d!\n", num);
+   
+   scanf("%c%*c", &pause); // To pause while a dangling newline is present
+   
    return 0;
-}
+} // MAIN END
